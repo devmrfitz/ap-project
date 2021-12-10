1eb81e860c3e24637bfe91329eccd5f051477735
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-12-10 16:29:14 +0530

    update

--- src/main/java/com/example/ap_project/menuPageController.java
+++ src/main/java/com/example/ap_project/menuPageController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 public class menuPageController {
 
@@ -15,17 +16,20 @@ public class menuPageController {
 
     @FXML
     void exitGame(ActionEvent event) {
-
+        System.out.println("exit clicked");
+//        Stage stage = (Stage) exitButton.getScene().getWindow();
+//        stage.close();
     }
 
     @FXML
     void gameStart(ActionEvent event) {
+        System.out.println("play clicked");
 
     }
 
     @FXML
     void loadGame(ActionEvent event) {
-
+        System.out.println("load clicked");
     }
 
     @FXML
