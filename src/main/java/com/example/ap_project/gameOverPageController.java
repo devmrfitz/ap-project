8c3413b306e310738fd10ca1b7ff8c4cc6f1e884
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-11 20:17:25 +0530

    GameOver Page Working

--- src/main/java/com/example/ap_project/gameOverPageController.java
+++ src/main/java/com/example/ap_project/gameOverPageController.java
@@ -1,5 +1,9 @@
 package com.example.ap_project;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -40,9 +45,57 @@ public class gameOverPageController {
 
     }
 
+
     @FXML
     void initialize() {
-        assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'gameOverPage.fxml'.";
+        welcome();
+    }
 
+    public void welcome() {
+        final Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        timeline.setAutoReverse(false);
+
+        final KeyFrame kf = new KeyFrame(Duration.millis(700), new KeyValue(pane.prefWidthProperty(), 501,
+                Interpolator.EASE_BOTH));
+        final KeyFrame kf2 = new KeyFrame(Duration.millis(700), new KeyValue(pane.prefHeightProperty(), 326,
+                Interpolator.EASE_BOTH));
+
+        final KeyFrame kf3 = new KeyFrame(Duration.millis(700), new KeyValue(pane.layoutXProperty(), 189,
+                Interpolator.EASE_BOTH));
+        final KeyFrame kf4 = new KeyFrame(Duration.millis(700), new KeyValue(pane.layoutYProperty(), 77,
+                Interpolator.EASE_BOTH));
+
+
+
+        timeline.getKeyFrames().add(kf);
+        timeline.getKeyFrames().add(kf2);
+        timeline.getKeyFrames().add(kf3);
+        timeline.getKeyFrames().add(kf4);
+        timeline.play();
+    }
+
+    public void farewell() {
+        final Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        timeline.setAutoReverse(false);
+
+        final KeyFrame kf = new KeyFrame(Duration.millis(700), new KeyValue(pane.prefWidthProperty(), 0,
+                Interpolator.EASE_BOTH));
+        final KeyFrame kf2 = new KeyFrame(Duration.millis(700), new KeyValue(pane.prefHeightProperty(), 0,
+                Interpolator.EASE_BOTH));
+
+        final KeyFrame kf3 = new KeyFrame(Duration.millis(700), new KeyValue(pane.layoutXProperty(), 450,
+                Interpolator.EASE_BOTH));
+        final KeyFrame kf4 = new KeyFrame(Duration.millis(700), new KeyValue(pane.layoutYProperty(), 240,
+                Interpolator.EASE_BOTH));
+
+
+
+        timeline.getKeyFrames().add(kf);
+        timeline.getKeyFrames().add(kf2);
+        timeline.getKeyFrames().add(kf3);
+        timeline.getKeyFrames().add(kf4);
+        timeline.play();
     }
 }
