7d8821271ef43c0e8dc9617f463172916a2d2e43
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-11 10:54:25 +0530

    animation

--- src/main/java/com/example/ap_project/gameController.java
+++ src/main/java/com/example/ap_project/gameController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
+import javafx.animation.PathTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
@@ -15,6 +16,9 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -39,14 +43,25 @@ public class gameController {
     }
 
     @FXML
-    void pause(MouseEvent event) throws IOException {
+    void pause(MouseEvent event) throws IOException, InterruptedException {
         System.out.println("Pause");
 //        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("pausePage.fxml")));
 //        stage = (Stage)((Node) (event.getSource())).getScene().getWindow();
 //        scene = new Scene(root);
 //        stage.setScene(scene);
 //        stage.show();
-        translateRectangle(hero, 0, -100, 2);
+//        for (int y=0;y<100;y++) {
+//            translateRectangle(hero, 0, -1, 0.02);
+//        }
+//        translateRectangle(hero, 0, -70, 2);
+//        Thread.sleep(2000);
+        translateRectangle(hero, 0, 70, 2);
+
+
+//        for (int y=0;y<100;y++) {
+//            translateRectangle(hero, 0, 1, 2/100);
+//        }
+
 
     }
 
@@ -57,15 +72,20 @@ public class gameController {
 
 
     //Translate a rectangle JAVAFx
-    public void translateRectangle(Region rectangle, double x, double y, int duration) {
-        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(duration), rectangle);
-        translateTransition.setByX(x);
-        translateTransition.setByY(y);
-        translateTransition.setCycleCount(18);
-        translateTransition.setRate(3);
-        translateTransition.setDelay(Duration.seconds(0));
-        translateTransition.setAutoReverse(true);
-        translateTransition.play();
+    public void translateRectangle(Region rectangle, double x, double y, double duration) {
+        PathTransition pathTransition = new PathTransition();
+        Path path = new Path();
+        path.getElements().add (new MoveTo(0f, 50f));
+        path.getElements().add (new CubicCurveTo(40f, 10f, 390f, 240f, 1904, 50f));
+
+        pathTransition.setDuration(Duration.millis(10000));
+        pathTransition.setNode(rectangle);
+        pathTransition.setPath(path);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(4);
+        pathTransition.setAutoReverse(true);
+
+        pathTransition.play();
     }
 
     //setRotate
