ec0c775750f7f7c14c712230cf0dec5cc8450179
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-01-01 15:12:10 +0530

    minor

--- src/main/java/com/example/ap_project/JumpHandler.java
+++ src/main/java/com/example/ap_project/JumpHandler.java
@@ -9,12 +9,22 @@ public class JumpHandler implements Runnable {
     private Jumpable jumpable;
     private ArrayList<Node> nodes;
 
+    public JumpHandler(Jumpable jumpable, ArrayList<Node> nodes) {
+        this.jumpable = jumpable;
+        this.nodes = nodes;
+    }
+
     public void run() {
         while (true) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+            // iterate through all nodes and check if they are in the same position as the jumpable
+            for (Node node : nodes) {
+                if (Utility.intersects((Node)jumpable, node))
+                    jumpable.jump();
             }
 
         }
