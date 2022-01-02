c788bf42c1a5ba77570d0dd3588f8435586c6956
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2022-01-02 10:08:05 +0530

    weapon

--- src/main/java/com/example/ap_project/Weapon.java
+++ src/main/java/com/example/ap_project/Weapon.java
@@ -1,6 +1,6 @@
 package com.example.ap_project;
 
-public class Weapon {
+public class Weapon implements Cloneable{
     private int level;
 
     public Weapon(){
@@ -25,5 +25,26 @@ public class Weapon {
 
     public int getLevel(){
         return level;
+    }
+
+    @Override
+    public Weapon clone() {
+        try {
+            Weapon clone = (Weapon) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public boolean equals(Object obj){
+        if (obj instanceof Weapon){
+            Weapon a = new Weapon();
+            return a.equals(obj);
+        }
+        else{
+            return false;
+        }
     }
 }
